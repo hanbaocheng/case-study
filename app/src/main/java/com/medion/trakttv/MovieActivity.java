c81720499e39974410b334281593aaa7e7c93883
Baocheng Han <baocheng.han@medion.com>
2016-07-15 15:14:39 +0200

    Delete PlaceholderFragment and UI

--- app/src/main/java/com/medion/trakttv/MovieActivity.java
+++ app/src/main/java/com/medion/trakttv/MovieActivity.java
@@ -3,7 +3,6 @@ package com.medion.trakttv;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -12,12 +11,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.medion.trakttv.data.MovieInfo;
 import com.medion.trakttv.receiver.DownloadResultReceiver;
@@ -93,10 +88,11 @@ public class MovieActivity extends AppCompatActivity implements
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
-        /* Starting Download Service */
+        /* Init ResultReceicer*/
         mDownloadReceiver = new DownloadResultReceiver(new Handler());
         mDownloadReceiver.setReceiver(this);
 
+        /* Starting Download Service */
         requestNextPage();
     }
 
@@ -145,52 +141,6 @@ public class MovieActivity extends AppCompatActivity implements
     }
 
     /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public void onCreate(@Nullable Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setHasOptionsMenu(true);
-        }
-
-        public void onPrepareOptionsMenu(Menu menu) {
-            MenuItem item=menu.findItem(R.id.action_search);
-            item.setVisible(false);
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_movie, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
-    /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
@@ -220,8 +170,8 @@ public class MovieActivity extends AppCompatActivity implements
 
         @Override
         public int getCount() {
-            // Show 2 total pages.
-            return 2;
+            // Show 1 total pages.
+            return 1;
         }
 
         @Override
@@ -239,8 +189,8 @@ public class MovieActivity extends AppCompatActivity implements
     @Override
     public void onListFragmentInteraction(MovieInfo movieInfo) {
         // Smooth scroll to detail fragment to show the detail
-        mViewPager.setCurrentItem(1,true);
-        mMovieDetailFragment.updateMovieDetail(movieInfo);
+/*        mViewPager.setCurrentItem(1,true);
+        mMovieDetailFragment.updateMovieDetail(movieInfo);*/
     }
 
     @Override
